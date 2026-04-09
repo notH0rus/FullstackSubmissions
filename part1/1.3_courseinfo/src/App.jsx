127dfd52c41b29f80f338c114f977c38b3541722
notH0rus <145000468+notH0rus@users.noreply.github.com>
2026-04-09 10:49:22 -0500

    Part 1.3 finished:D

---
+++ part1/1.3_courseinfo/src/App.jsx
@@ -0,0 +1,62 @@
+const Header = (props) => {
+  console.log(props)
+  return (<h1>{props.course}</h1>)
+}
+
+const Content = (props) => {
+  console.log(props)
+  return (
+    <>
+      <Part name={props.name1} exercises={props.exercises1} />
+      <Part name={props.name2} exercises={props.exercises2} />
+      <Part name={props.name3} exercises={props.exercises3} />
+    </>
+  )
+}
+
+const Total = (props) => {
+  console.log(props)
+  return (<p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>)
+}
+
+const Part = (props) => {
+  console.log(props)
+  return (
+    <p>
+      {props.name} {props.exercises}
+    </p>
+  )
+}
+
+const App = () => {
+
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content
+        name1={part1.name} exercises1={part1.exercises}
+        name2={part2.name} exercises2={part2.exercises}
+        name3={part3.name} exercises3={part3.exercises}
+      />
+      <Total
+        exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises}
+      />
+    </div>
+  )
+}
+
+export default App
